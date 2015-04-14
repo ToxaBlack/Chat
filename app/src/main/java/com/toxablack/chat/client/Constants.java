97f88e4d6161e23c301c0ef4784b995582593db3
toxa_black <toxablack@gmail.com>
2015-04-15 00:09:41 +0300

    ver1

---
+++ app/src/main/java/com/toxablack/chat/client/Constants.java
@@ -0,0 +1,15 @@
+package com.toxablack.chat.client;
+
+
+public interface Constants {
+
+    /**
+     * Base URL of the Demo Server (such as http://my_host:8080/gcm-demo)
+     */
+	String SERVER_URL = "http://toxamessenger.appspot.com";
+
+    /**
+     * Google API project id registered to use GCM.
+     */
+	String SENDER_ID = "295970793840";
+}
