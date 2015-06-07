ecdd4a6cd9a9e657c734b2704cbf6984c3be4982
toxa_black <toxablack@gmail.com>
2015-06-07 18:44:40 +0300

    ver 3
    change the order

--- app/src/main/java/com/toxablack/chat/MessagesFragment.java
+++ app/src/main/java/com/toxablack/chat/MessagesFragment.java
@@ -3,6 +3,7 @@ package com.toxablack.chat;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import android.app.Activity;
@@ -18,130 +19,129 @@ import android.widget.LinearLayout;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
-/**
- * A fragment representing a list of Items.
- * <p />
- * <p />
- * Activities containing this fragment MUST implement the {@link Callbacks}
- * interface.
- * 
- *
- */
+
 public class MessagesFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
-	
-	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	private static final DateFormat[] df = new DateFormat[] {
-		DateFormat.getDateInstance(), DateFormat.getTimeInstance()};
 
-	private OnFragmentInteractionListener mListener;
-	private SimpleCursorAdapter adapter;
-	private Date now;
-	
-	@Override
-	public void onAttach(Activity activity) {
-		super.onAttach(activity);
-		try {
-			mListener = (OnFragmentInteractionListener) activity;
-		} catch (ClassCastException e) {
-			throw new ClassCastException(activity.toString() + " must implement OnFragmentInteractionListener");
-		}
-	}	
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final DateFormat[] DATE_FORMATS = new DateFormat[]{
+            DateFormat.getDateInstance(), DateFormat.getTimeInstance()};
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		now = new Date();
-		
-		adapter = new SimpleCursorAdapter(getActivity(), 
-				R.layout.chat_list_item, 
-				null, 
-				new String[]{DataProvider.COL_MSG, DataProvider.COL_AT}, 
-				new int[]{R.id.text1, R.id.text2},
-				0);
-		
-		adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
-			
-			@Override
-			public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
-				switch(view.getId()) {
-				case R.id.text1:
-					LinearLayout root = (LinearLayout) view.getParent().getParent();
-					if (cursor.getString(cursor.getColumnIndex(DataProvider.COL_FROM)) == null) {
-						root.setGravity(Gravity.RIGHT);
-						root.setPadding(50, 10, 10, 10);
-					} else {
-						root.setGravity(Gravity.LEFT);
-						root.setPadding(10, 10, 50, 10);
-					}
-					break;
-					
-				case R.id.text2:
-					TextView tv = (TextView) view;
-					tv.setText(getDisplayTime(cursor.getString(columnIndex)));
-					return true;					
-				}
-				return false;
-			}
-		});		
-		
-		setListAdapter(adapter);
-	}	
+    private OnFragmentInteractionListener mListener;
+    private SimpleCursorAdapter mAdapter;
+    private Date mNow;
+    private Calendar mCalendar;
 
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-		
-		getListView().setDivider(null);
-		
-		Bundle args = new Bundle();
-		args.putString(DataProvider.COL_EMAIL, mListener.getProfileEmail());
-		getLoaderManager().initLoader(0, args, this);
-	}
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnFragmentInteractionListener");
+        }
+    }
 
-	@Override
-	public void onDetach() {
-		super.onDetach();
-		mListener = null;
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mNow = new Date();
+        mCalendar = Calendar.getInstance();
+        mCalendar.setTime(mNow);
 
-	public interface OnFragmentInteractionListener {
-		public String getProfileEmail();
-	}
-	
-	private String getDisplayTime(String datetime) {
-		try {
-			Date dt = sdf.parse(datetime);
-			if (now.getYear()==dt.getYear() && now.getMonth()==dt.getMonth() && now.getDate()==dt.getDate()) {
-				return df[1].format(dt);
-			}
-			return df[0].format(dt);
-		} catch (ParseException e) {
-			return datetime;
-		}
-	}
-	
-	//----------------------------------------------------------------------------
+        mAdapter = new SimpleCursorAdapter(getActivity(),
+                R.layout.chat_list_item,
+                null,
+                new String[]{DataProvider.COL_MSG, DataProvider.COL_AT},
+                new int[]{R.id.text1, R.id.text2},
+                0);
 
-	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		String profileEmail = args.getString(DataProvider.COL_EMAIL);
-		CursorLoader loader = new CursorLoader(getActivity(), 
-				DataProvider.CONTENT_URI_MESSAGES, 
-				null, 
-				DataProvider.COL_FROM + " = ? or " + DataProvider.COL_TO + " = ?",
-				new String[]{profileEmail, profileEmail}, 
-				DataProvider.COL_AT + " DESC"); 
-		return loader;
-	}
+        mAdapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
 
-	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-		adapter.swapCursor(data);
-	}
+            @Override
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                switch (view.getId()) {
+                    case R.id.text1:
+                        LinearLayout root = (LinearLayout) view.getParent().getParent();
+                        if (cursor.getString(cursor.getColumnIndex(DataProvider.COL_FROM)) == null) {
+                            root.setGravity(Gravity.RIGHT);
+                            root.setPadding(50, 10, 10, 10);
+                        } else {
+                            root.setGravity(Gravity.LEFT);
+                            root.setPadding(10, 10, 50, 10);
+                        }
+                        break;
 
-	@Override
-	public void onLoaderReset(Loader<Cursor> loader) {
-		adapter.swapCursor(null);
-	}
+                    case R.id.text2:
+                        TextView tv = (TextView) view;
+                        tv.setText(getDisplayTime(cursor.getString(columnIndex)));
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        setListAdapter(mAdapter);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        getListView().setDivider(null);
+
+        Bundle args = new Bundle();
+        args.putString(DataProvider.COL_EMAIL, mListener.getProfileEmail());
+        getLoaderManager().initLoader(0, args, this);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        public String getProfileEmail();
+    }
+
+    private String getDisplayTime(String datetime) {
+        try {
+            Date date = SIMPLE_DATE_FORMAT.parse(datetime);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            if (mCalendar.get(Calendar.YEAR) == calendar.get(Calendar.YEAR)
+                    && mCalendar.get(Calendar.MONTH) == calendar.get(Calendar.MONTH)
+                    && mCalendar.get(Calendar.DATE) == calendar.get(Calendar.DATE)) {
+                return DATE_FORMATS[1].format(date);
+            }
+            return DATE_FORMATS[0].format(date);
+        } catch (ParseException e) {
+            return datetime;
+        }
+    }
+
+    //----------------------------------------------------------------------------
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String profileEmail = args.getString(DataProvider.COL_EMAIL);
+        CursorLoader loader = new CursorLoader(getActivity(),
+                DataProvider.CONTENT_URI_MESSAGES,
+                null,
+                DataProvider.COL_FROM + " = ? or " + DataProvider.COL_TO + " = ?",
+                new String[]{profileEmail, profileEmail},
+                DataProvider.COL_AT + " ASC");
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
 
 }
--- app/src/main/java/com/toxablack/chat/client/Constants.java
+++ app/src/main/java/com/toxablack/chat/client/Constants.java
@@ -6,10 +6,10 @@ public interface Constants {
     /**
      * Base URL of the Demo Server (such as http://my_host:8080/gcm-demo)
      */
-	String SERVER_URL = "http://my_host:8080/gcm-demo";
+	String SERVER_URL = "http://toxamessenger.appspot.com";
 
     /**
      * Google API project id registered to use GCM.
      */
-	String SENDER_ID = "";
+	String SENDER_ID = "295970793840";
 }
